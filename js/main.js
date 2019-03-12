73cf80b43e8b89ab08860f73cbffde8abe8054c2
Ekaterina Mikhailova <19kotik98@gmail.com>
2019-03-12 15:55:57 +0300

    со свайпером

--- js/main.js
+++ js/main.js
@@ -23,21 +23,6 @@ window.onload = function () {
       showModal: false,
       slide: 0,
       sliding: null,
-      swiperOption: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }
-      }
     },
     methods: {
       onSlideStart(slide) {
